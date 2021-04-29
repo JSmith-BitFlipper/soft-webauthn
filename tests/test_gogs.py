f2a7b2b94f6cd17ff8083b913ce1fc616fea00a3
Damian Barabonkov <damianb@mit.edu>
2021-04-29 18:42:28 -0400

    Gogs HTTP tester works with multiple concurrent users

--- tests/test_gogs.py
+++ tests/test_gogs.py
@@ -1,20 +1,25 @@
 """SoftWebauthnDevice usage for (yubico's) fido2 example webserver/application"""
 
+# Disable the HTTPs verify warning
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 import requests
 import pytest
 import json
+import pickle
+
+from multiprocessing import Process, Queue
+import time
+import os
+
+import numpy as np
 
 # Hacky way to import `soft_webauthn`
 import sys
 sys.path.append("..")
 
 from soft_webauthn import SoftWebauthnDevice
-
-cookies = dict()
-device = SoftWebauthnDevice()
 
 def findTrim(s, findStr):
     return s.find(findStr) + len(findStr)
@@ -34,119 +39,241 @@ def post(url, data, cookies=dict()):
                          cookies=cookies,
                          verify=False)
 
-def test_gogs_server_login():
-    global cookies
+class User():
+    def __init__(self, username, password):
+        self.cookies = dict()
+        
+        # Remember the input arguments
+        self.username = username
+        self.password = password
 
-    resp = get('/user/login')
+        # Generate a new device for this `User`
+        self.device = SoftWebauthnDevice()
+        
+        # Log the user in and webauthn register them (if necessary)
+        self.log_in_and_register()
 
-    # Set the `i_like_gogs` session cookie
-    setCookie = resp.headers['Set-Cookie']
-    cookies['i_like_gogs'] = getCookie(setCookie, 'i_like_gogs')
+    def log_in_and_register(self):
+        self.gogs_server_login()
 
-    resp = post('/user/login',
-                {"user_name": (None, "durian"),
-                 "username": (None, "durian"),
-                 "password": (None, "maga2020!"),
-                 "assertion": (None, "no-assertion"),
-                },
-                cookies=cookies,
-    )
+        # Register only if the user does not have webauthn enabled
+        if not self.is_webauthn_enabled():
+            self.gogs_server_registration()
 
-    if resp.status_code != 200:
-        print("Error in login")
-        return
+    def is_webauthn_enabled(self):
+        resp = get('/webauthn/is_enabled/{}'.format(self.username))
 
-    resp = get('/')
+        if resp.status_code != 200:
+            print("Error with webauthn is enabled")
+            return False
 
-    # Set the `csrf` cookie
-    setCookie = resp.headers['Set-Cookie']
-    cookies['_csrf'] = getCookie(setCookie, '_csrf')
+        return resp.json()['webauthn_is_enabled']
 
-    print("Successfully logged in user!")
+    def gogs_server_login(self):
+        # Get the `i_like_gogs` session cookie
+        resp = get('/user/login')
 
-def test_gogs_server_registration():  # pylint: disable=redefined-outer-name
-    """Registration example"""
-    global cookies
-    global device
+        # Set the `i_like_gogs` session cookie
+        setCookie = resp.headers['Set-Cookie']
+        self.cookies['i_like_gogs'] = getCookie(setCookie, 'i_like_gogs')
 
-    # Begin registration
-    resp = post('/webauthn/begin_register',
-                {"username": (None, "durian")},
-                cookies=cookies
-    )
+        attestation = "no-assertion"
 
-    if resp.status_code != 200:
-        print("Error in begin_register")
-        return
+        # If webauthn is enabled, generate an `assertion` object
+        if self.is_webauthn_enabled():
+            resp = post('/webauthn/begin_login',
+                        {"username": (None, self.username)},
+                        cookies=self.cookies,
+            )
 
-    # Set the `webauthn-session` cookie
-    setCookie = resp.headers['Set-Cookie']
-    cookies['webauthn-session'] = getCookie(setCookie, 'webauthn-session')
+            if resp.status_code != 200:
+                print("Error in begin_login")
+                return
 
-    options = {'publicKey': resp.json()}
+            # Set the `webauthn-session` cookie
+            setCookie = resp.headers['Set-Cookie']
+            self.cookies['webauthn-session'] = getCookie(setCookie, 'webauthn-session')
 
-    # User holds an authenticator.
-    #
-    # NOTE: SoftWebauthnDevice emulates mixed client and authenticator behavior
-    # and can be used for testing Webauthn/FIDO2 enabled applications during
-    # continuous integration test-cases.
-    attestation = device.create(options, 'https://localhost:8081')
+            options = {'publicKey': resp.json()}
 
-    resp = post('/webauthn/finish_register',
-                {"username": (None, "durian"),
-                 "credentials": (None, json.dumps(attestation)),
-                },
-                cookies=cookies,
-    )
+            #  User holds an authenticator
+            attestation = self.device.get(options, 'https://localhost:8081')
 
-    if resp.status_code == 200:
-        print("Successfully registered webauthn user!")
-    else:
-        print("Error in register webauthn user")
+        resp = post('/user/login',
+                    {"user_name": (None, self.username),
+                     "username": (None, self.username),
+                     "password": (None, self.password),
+                     "assertion": (None, json.dumps(attestation)),
+                    },
+                    cookies=self.cookies,
+        )
 
-def test_gogs_server_change_email():
-    global cookies
+        if resp.status_code != 200:
+            print("Error in login")
+            return
 
-    auth_text = "Confirm profile details: username durian email test@email.comsada"
+        resp = get('/')
 
-    resp = post('/webauthn/begin_attestation',
-                {"auth_text": (None, auth_text)},
-                cookies=cookies
-    )
+        # Set the `csrf` cookie
+        setCookie = resp.headers['Set-Cookie']
+        self.cookies['_csrf'] = getCookie(setCookie, '_csrf')
 
-    if resp.status_code != 200:
-        print("Error in begin_attestation")
-        return
+        print("Successfully logged in user!")
 
-    # Set the `webauthn-session` cookie
-    setCookie = resp.headers['Set-Cookie']
-    cookies['webauthn-session'] = getCookie(setCookie, 'webauthn-session')
+    def gogs_server_registration(self):  # pylint: disable=redefined-outer-name
+        """Registration example"""
+        # Begin registration
+        resp = post('/webauthn/begin_register',
+                    {"username": (None, self.username)},
+                    cookies=self.cookies
+        )
+        
+        if resp.status_code != 200:
+            print("Error in begin_register")
+            return
 
-    options = {'publicKey': resp.json()}
+        # Set the `webauthn-session` cookie
+        setCookie = resp.headers['Set-Cookie']
+        self.cookies['webauthn-session'] = getCookie(setCookie, 'webauthn-session')
 
-    #  User holds an authenticator
-    attestation = device.get(options, 'https://localhost:8081')
+        options = {'publicKey': resp.json()}
 
-    resp = post('/user/settings',
-                {"_csrf": (None, cookies['_csrf']),
-                 "name": (None, "durian"),
-                 "full_name": (None, ""),
-                 "email": (None, "test@email.comsada"),
-                 "website": (None, ""),
-                 "location": (None, ""),
-                 "auth_text": (None, auth_text),
-                 "assertion": (None, json.dumps(attestation)),
-                },
-                cookies=cookies,
-    )
+        # User holds an authenticator.
+        #
+        # NOTE: SoftWebauthnDevice emulates mixed client and authenticator behavior
+        # and can be used for testing Webauthn/FIDO2 enabled applications during
+        # continuous integration test-cases.
+        attestation = self.device.create(options, 'https://localhost:8081')
 
-    if resp.status_code == 200:
-        print("Successfully changed user email!")
-    else:
-        print("Error in change user email")
+        resp = post('/webauthn/finish_register',
+                    {"username": (None, self.username),
+                     "credentials": (None, json.dumps(attestation)),
+                    },
+                    cookies=self.cookies,
+        )
+
+        if resp.status_code == 200:
+            print("Successfully registered webauthn user!")
+        else:
+            print("Error in register webauthn user")
+
+    def gogs_server_change_email(self, newEmail):
+        auth_text = "Confirm profile details: username {} email {}".format(self.username, newEmail)
+
+        resp = post('/webauthn/begin_attestation',
+                    {"auth_text": (None, auth_text)},
+                    cookies=self.cookies
+        )
+
+        if resp.status_code != 200:
+            print("Error in begin_attestation")
+            return
+
+        # Set the `webauthn-session` cookie
+        setCookie = resp.headers['Set-Cookie']
+        self.cookies['webauthn-session'] = getCookie(setCookie, 'webauthn-session')
+
+        options = {'publicKey': resp.json()}
+
+        #  User holds an authenticator
+        attestation = self.device.get(options, 'https://localhost:8081')
+
+        # Time this POST request
+        t0 = time.time()
+        resp = post('/user/settings',
+                    {"_csrf": (None, self.cookies['_csrf']),
+                     "name": (None, self.username),
+                     "full_name": (None, ""),
+                     "email": (None, newEmail),
+                     "website": (None, ""),
+                     "location": (None, ""),
+                     "auth_text": (None, auth_text),
+                     "assertion": (None, json.dumps(attestation)),
+                    },
+                    cookies=self.cookies,
+        )
+        elapsed_time = time.time() - t0
+
+        if resp.status_code != 200:
+            print("Error in change user email")
+
+        # In milliseconds
+        return elapsed_time * 1000
+
+def change_emails(user, n_iters, queue):
+    run_times = []
+
+    for i in range(n_iters):
+        rt = user.gogs_server_change_email("email@email{}.com".format(i))
+        run_times.append(rt)
+
+        if i % 100 == 0:
+            print("=== User {} \t Iters {} \t Avg time: {} ms ===".format(user.username, i, np.mean(run_times)))
+
+    # Place all of the `run_times` into the `queue`
+    for rt in run_times:
+        queue.put(rt)
 
 if __name__ == '__main__':
-    test_gogs_server_login()
-    test_gogs_server_registration()
-    test_gogs_server_change_email()
+    n_users = 8
+    n_iters = 500
+    percentile = 95
 
+    users = []
+
+    # Load the `users` if they exist
+    usersFiles = './users.pkl'
+    if os.path.exists(usersFiles):
+        with open(usersFiles, 'rb') as infile:
+            users = pickle.load(infile)
+    else:
+        # Initialize the new `User`s
+        for u in range(n_users):
+            username = 'durian{}'.format(u)
+            users.append(User(username, 'password'))
+
+    # Initialize a `queue` to collect all of the run times
+    queue = Queue()
+    procs = [Process(target=change_emails, 
+                     kwargs={'user': users[u], 'n_iters': n_iters, 'queue': queue}) 
+             for u in range(n_users)]
+    # procs = []
+    # for u in range(n_users):
+    #     user = users[0]
+
+    #     def target():
+    #         print("User: ", user.username)
+    #         change_emails(user, n_iters, queue)
+
+    #     procs.append(Process(target=target))
+
+    for p in procs:
+        p.start()
+
+    for p in procs:
+        p.join()
+
+    # Collect the `run_times`
+    run_times = []
+    while not queue.empty():
+        run_times.append(queue.get())
+
+    #print(run_times)
+    
+    # Get the `percentile` from the `run_times`
+    print("{}th percentile: {}".format(
+        percentile,
+        np.percentile(run_times, percentile))
+    )
+    
+    # durian = users['durian']
+    # print(durian.is_webauthn_enabled())
+
+    # for i in range(100):
+    #     durian.gogs_server_change_email("email@email{}.com".format(i))
+    
+    # TODO: Pickle does not want to serialize the private_key
+    #
+    # Write the `users` to file
+    #with open(usersFiles, 'wb') as outfile:
+    #    pickle.dump(users, outfile)
